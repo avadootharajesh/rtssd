e408191aab6d9c81bf9b0b88fedc472c79051038
avadootharajesh <avadootha.rajesh.netha@gmail.com>
2025-07-26 23:30:06 +0530

    deploy scripts name backend

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.use(bodyparser.json());
 
 // Definitions of varables and routes
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.BACKEND_PORT || 5001;
 
 // Connect to MongoDB
 
